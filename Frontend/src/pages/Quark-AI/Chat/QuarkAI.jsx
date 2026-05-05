c61aa7855a6cd2966faa6b732afbf293abfcf165
Anas-Odeh465 <aodeh462@gmail.com>
2026-05-05 15:06:24 +0300

    fix error model not found

--- Frontend/src/pages/Quark-AI/Chat/QuarkAI.jsx
+++ Frontend/src/pages/Quark-AI/Chat/QuarkAI.jsx
@@ -514,8 +514,8 @@ useEffect(() => {
                 <div className={`flex flex-row items-center justify-center p-1 px-2 rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out ${isDarkMode ? 'bg-[#27292d]  hover:bg-[#1f2023]' : 'bg-white hover:bg-gray-100'}`}>
                   <BotIcon className={`w-4 h-4 cursor-pointer ${isDarkMode ? 'text-white' : 'text-black'}`}/>
                   <select name="model-selector" id="model-selector" value={selectedModel} onChange={(e) => setSelectedModel(e.target.value)} className={`rounded-xl ${isDarkMode ? "bg-black" : "bg-white text-black"}`}>
-                    <option value="GPT-4o-mini">GPT-4o-mini</option>
-                    <option value="GPT-4.1">GPT-4.1</option>
+                    <option value="gpt-4o-mini">GPT-4o-mini</option>
+                    <option value="gpt-4.1">GPT-4.1</option>
                   </select>
                 </div>
                 <div className={`hidden xl:flex lg:flex flex-row items-center justify-center p-1 px-2 rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out ${isDarkMode ? 'bg-[#27292d]  hover:bg-[#1f2023]' : 'bg-white hover:bg-gray-100'}`}>
